f803a56b8e17387e1a5652f01b1c0e657d122c70
SoyLuisCastro <86686595+SoyLuisCastro@users.noreply.github.com>
2021-09-08 18:51:50 -0500

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,78 @@
+<?php 
+session_start();
+?>
+<?php require_once 'includes/conexion.php'; ?> <!--incluimos la coneccion a cabecera-->
+<?php require_once 'includes/helpers.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de comidas</title>
+   
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body style="background-color: antiquewhite  ;">
+    <!--CABECERA-->
+    <header id="cabecera">
+        <!--LOGO-->
+        <div id="logo">
+            <a href="index.php">
+                <a style="background-color:whitesmoke;">Blog Gastronomico</a>
+            </a>
+        </div>
+
+        <!--MENU-->
+        
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+
+                <?php 
+                    $continentes = conseguirContinentes($db);
+                    if(!empty($continentes)):
+  
+                            while($continente = mysqli_fetch_assoc($continentes)) :
+                ?>
+                            <li>
+                                <a href="continente.php?id=<?=$continente['id']?>"><?=$continente['nombre']?></a>
+                            </li>
+
+                <?php 
+                            endwhile; 
+                    endif;
+                ?>
+                <li>
+                    <a href="videos/videos.php">videos</a>
+                </li>
+                <li>
+                    <a href="recetas/recetas.php">recetas </a>
+                </li>
+                <li>
+                    <a href="comidac/comidac.php">recetas comida criolla</a>
+                </li>
+                <li>
+                    <a href="aderezo/aderezo.php">aderezos</a>
+                </li>
+                <li>
+                    <a href="salsas/salsas.php">salsas</a>
+                </li>
+                <li>
+                    <a href="contactanos/contactanos.php">contactanos</a>
+                </li>
+                    
+                 
+            </ul>
+        </nav>
+
+        <div class="clearfix"></div>
+
+    </header>
+
+    <div id="contenedor">
---
+++ pie.php
@@ -0,0 +1,10 @@
+<div class="clearfix"></div>
+    </div> <!--fin del contenedor, la parte de abajo, el pie de pagina -->   
+    
+    <!--FOOTER-->
+    <footer id="pie">
+        <p> Luis castro <br>
+            instituto supeior tecnológico JBA </p>
+    </footer>
+</body>
+</html>
